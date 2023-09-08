6ec7ab5f9cbe231991e2e9819ee6e6babd32795a
LyashenkoKV <Lyashenko@inbox.ru>
2023-09-08 10:53:47 +0300

    Create UI

---
+++ Translator/Controller/LanguageViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LanguageViewController.swift
+//  Translator
+//
+//  Created by Konstantin Lyashenko on 07.09.2023.
+//
+
+import UIKit
+
+class LanguageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+}
